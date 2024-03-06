c8cb4119049f038bbc15b9ff0082459eae04e2c1
Ernest Staszuk <ernest.staszuk@eobuwie.com.pl>
2024-03-07 00:46:09 +0100

    A-04 paging and sorting

--- src/main/kotlin/com/vujade/crudzik/adapter/db/CampaignRepository.kt
+++ src/main/kotlin/com/vujade/crudzik/adapter/db/CampaignRepository.kt
@@ -2,8 +2,9 @@ package com.vujade.crudzik.adapter.db
 
 import com.vujade.crudzik.notdomain.Campaign
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.rest.webmvc.RepositoryRestController
 import java.util.UUID
 
 @RepositoryRestController
-interface CampaignRepository: CrudRepository<Campaign, UUID>
+interface CampaignRepository: CrudRepository<Campaign, UUID>, PagingAndSortingRepository<Campaign, UUID>

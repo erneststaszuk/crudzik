26c009c04e57d7a8b2747eb6cd83df9c3e793d9b
Ernest Staszuk <ernest.staszuk@eobuwie.com.pl>
2024-03-06 20:47:26 +0100

    A-01 initial commit

---
+++ src/main/kotlin/com/vujade/crudzik/CrudzikApplication.kt
@@ -0,0 +1,11 @@
+package com.vujade.crudzik
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudzikApplication
+
+fun main(args: Array<String>) {
+    runApplication<CrudzikApplication>(*args)
+}

b511da0f7f493317f89a2cb249f500f9f88debb0
Ernest Staszuk <ernest.staszuk@eobuwie.com.pl>
2024-03-07 00:01:09 +0100

    A-02 Entities

---
+++ src/main/kotlin/com/vujade/crudzik/notdomain/Campaign.kt
@@ -0,0 +1,15 @@
+package com.vujade.crudzik.notdomain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import java.time.LocalDate
+import java.util.UUID
+
+@Table("campaign")
+data class Campaign(
+    @Id val id: UUID?,
+    val accountId: UUID,
+    val name: String,
+    val dateStart: LocalDate,
+    val dateEnd: LocalDate,
+)

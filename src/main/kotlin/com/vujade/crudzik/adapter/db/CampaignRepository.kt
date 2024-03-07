e0588ea5c701712d5111d8a4743eb49eeba4f8b1
Ernest Staszuk <ernest.staszuk@eobuwie.com.pl>
2024-03-07 01:39:53 +0100

    A-05 custom query

--- src/main/kotlin/com/vujade/crudzik/adapter/db/CampaignRepository.kt
+++ src/main/kotlin/com/vujade/crudzik/adapter/db/CampaignRepository.kt
@@ -1,10 +1,14 @@
 package com.vujade.crudzik.adapter.db
 
 import com.vujade.crudzik.notdomain.Campaign
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.rest.webmvc.RepositoryRestController
 import java.util.UUID
 
 @RepositoryRestController
-interface CampaignRepository: CrudRepository<Campaign, UUID>, PagingAndSortingRepository<Campaign, UUID>
+interface CampaignRepository: CrudRepository<Campaign, UUID>, PagingAndSortingRepository<Campaign, UUID> {
+  fun findAllByAccountId(accountId: UUID, pageable: Pageable): Page<Campaign>
+}
